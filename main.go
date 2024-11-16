d0098c3a89d38941953c3dab48456402c55e4eff
Kaska <kmiskolczi@pagerduty.com>
2024-11-15 21:41:49 -0500

    initial template go ai project

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ import (
 	changelog "github.com/octokas/go-ai/scripts/changelog"
 	fix_yaml_comments "github.com/octokas/go-ai/scripts/fix_yaml_comments"
 	run_tests "github.com/octokas/go-ai/tests/run_tests"
-	test_reporter "github.com/octokas/go-ai/tests/test_reporter"
 )
 
 func main() {
 	changelog.GenerateChangelog()
 	fix_yaml_comments.FixYAMLComments(".")
 	run_tests.RunTests()
-	test_reporter.SaveTestReports()
 
 	api.RunAPI()
 	worker.RunWorker()
